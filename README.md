7956e3e62f890399f2b4d5444daeb54d74343401
NathanJacquot93 <Nathan@MacBook-Pro-de-Nathan.local>
2017-01-27 11:02:59 +0100

    ajout des fichiers de base

---
+++ README.md
@@ -0,0 +1,17 @@
+Hello world app with tests needed
+=================================
+
+How to install
+--------------
+
+```
+composer install
+```
+
+
+How to run
+----------
+
+```
+php -S localhost:8000 -t web
+```
---
+++ src/Controller/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace MMI\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class MainController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('index.html.twig');
+    }
+}
